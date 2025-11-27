e359d9136566e93790cc9e94d544a3a1a6fb7e36
Albert Sanchez <albert.san.iza@gmail.com>
2025-11-27 12:28:44 -0800

    Add AuthLoading wrapper with Spinner to enhance loading state

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,9 @@
 import { SignInButton } from '@clerk/clerk-react'
 import { createFileRoute, Link } from '@tanstack/react-router'
-import { Authenticated, Unauthenticated } from 'convex/react'
+import { Authenticated, AuthLoading, Unauthenticated } from 'convex/react'
 
 import { Button } from '@/components/ui/button'
+import { Spinner } from '@/components/ui/spinner'
 
 export const Route = createFileRoute('/')({
     component: RouteComponent
@@ -12,6 +13,11 @@ function RouteComponent() {
     return (
         <div className="fixed inset-0 flex flex-col items-center justify-center gap-6 p-6 text-8xl">
             <div>EASY DECK</div>
+            <AuthLoading>
+                <Button disabled>
+                    <Spinner />
+                </Button>
+            </AuthLoading>
             <Unauthenticated>
                 <Button asChild>
                     <SignInButton />
